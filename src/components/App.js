52626d2962fed515fde5ddb640f8668d8802d700
Datta-070 <chinmaydatt003@gmail.com>
2024-01-19 19:40:50 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,16 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+    const weather={
+      temperature:19,
+      conditions:"Sunny"
+    }
+
   return (
+
     <div>
-        {/* Do not remove the main div */}
+        <p style={weather.temperature>20 ? {backgroundColor:'red'} : {backgroundColor:'blue'}}>Temperature:{weather.temperature}</p>
+        <span>Condition: {weather.conditions}</span>
     </div>
   )
 }
